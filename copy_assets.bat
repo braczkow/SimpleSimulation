3221d6f7a33c99705943c8bfdf890b66b1be9f6b
Bartosz Raczkowski <Bartosz Raczkowski>
2016-01-14 17:01:37 +0100

    Added freeglut asset. Added jsoncpp-dist asset.

--- copy_assets.bat
+++ copy_assets.bat
@@ -1,8 +1,18 @@
-set external_dir=./source/external
+set external_dir=source\external
+set assets_dir=assets
 
-echo %external_dir%
+echo "external dir: " %external_dir%
+echo "assets dir: " %assets_dir%
 
-mkdir %external_dir%/Box2D
-xcopy assets\Box2D-master\Box2D %external_dir%/Box2D /s
+if not exist %external_dir% (
+mkdir %external_dir%
+) 
 
+mkdir %external_dir%\Box2D
+xcopy %assets_dir%\Box2D-master\Box2D %external_dir%\Box2D /s
 
+mkdir %external_dir%\jsoncpp
+xcopy %assets_dir%\jsoncpp-dist %external_dir%\jsoncpp /s
+
+mkdir %external_dir%\freeglut
+xcopy %assets_dir%\freeglut %external_dir%\freeglut /s
